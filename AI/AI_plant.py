c49e49547110fb4eb8713ed7ff2e425faf400336
koolmakmak <66991041@kmitl.ac.th>
2025-07-26 01:11:47 +0700

    connect-front-back

---
+++ AI/AI_plant.py
@@ -0,0 +1,4 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+df = pd.read_csv('plant.csv')
---
+++ frontend/user-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <iframe src="/App.html" width="100%" height="720px"/>
+    </div>
+  )
+}
+
+export default App
