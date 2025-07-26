a2c626255b5057b25c06c2726be29bec71478e42
koolmakmak <66991041@kmitl.ac.th>
2025-07-26 22:55:56 +0700

    fix_bug(a little bit)

--- frontend/user-app/src/App.js
+++ frontend/user-app/src/App.js
@@ -3,7 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <iframe src="/App.html" width="100%" height="720px"/>
+      <iframe
+        src="/App.html"
+        width="100%"
+        height="100%"
+        style={{ border: 'none', minHeight: '100vh' }}
+        title="Static HTML"
+      />
     </div>
   )
 }
