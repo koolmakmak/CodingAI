365258373f1fc564041ce30e5efa4c4587a79711
koolmakmak <66991041@kmitl.ac.th>
2025-07-27 17:41:37 +0700

    make less confusing file

--- AI/AI_plant.py
+++
@@ -1,4 +0,0 @@
-from sklearn.model_selection import train_test_split
-import pandas as pd
-
-df = pd.read_csv('plant.csv')
--- frontend/user-app/src/App.js
+++ frontend/user-app/src/App.js
@@ -4,11 +4,8 @@ const App = () => {
   return (
     <div>
       <iframe
-        src="/App.html"
-        width="100%"
-        height="100%"
-        style={{ border: 'none', minHeight: '100vh' }}
-        title="Static HTML"
+        src="/index.html"
+        title="User App"
       />
     </div>
   )
